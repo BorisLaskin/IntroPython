702a762320fd537162bc0150aa04f429d8ae316d
BorisLaskin <varislaskin@list.ru>
2023-05-05 18:54:07 +0300

    Lect1

---
+++ Lections/Lec1/Ex1/main.py
@@ -0,0 +1,36 @@
+# print("hello")
+
+# a = 5
+# b = None
+# print(a,b)
+# print(type(a),type(b))
+
+# s='hello'
+# w="world"
+# print(s,w)
+# print(s+w)
+
+# a = 1,45
+# b = 0
+# c = -5
+# print(bool(a),bool(b),bool(c))
+
+# d=float("5.45")
+# print(d)
+# print(type(a),type(d),int(d))
+
+a= 5.89996
+b = 1231.123123
+
+print(round(a,2))
+print(2>3)
+
+for i in range(0,7,2):
+    print(i)
+
+text = 'СъЕШЬ ещё этих МяГкИх французских булок'
+print(len(text)) # 39
+print('ещё' in text)
+print(text.upper()) # СЪЕШЬ ЕЩЁ ЭТИХ МЯГКИХ ФРАНЦУЗСКИХ БУЛОК
+print(text[-1])
+print(text[0:len(text):2])
---
+++ Lections/Lec2/main.py
@@ -0,0 +1,59 @@
+# list_1 = [] # Создание пустого списка
+# list_2 = list() # Создание пустого списка
+# list_1 = [7, 9, 11, 'safaf', 15, 17]
+
+# for item in list_1:
+#     print(item)
+#     print(list_1[0])
+
+# print(*list_1)
+# print(len(list_1))
+
+# list_2 = [1,5]
+# print(list_2)
+# list_2.append(9)
+# print(list_2)
+
+
+# list_1 = [12, 7, -1, 21, 0]
+# print(list_1.pop(0)) # 12
+# print(list_1) # [7, -1, 21, 0]
+
+# print(list_1.insert(2, 11))
+# print(list_1)
+# list_1.insert(10, 11)
+# print(list_1)
+
+t = () # создание пустого кортежа
+print(type(t)) # class <'tuple'>
+t = 1, 5, 3 
+print(type(t))
+print(t[1])
+t = (1)
+print(type(t))
+t = (28, 9, 1990)
+print(t) # (28, 9, 1990)
+print(type(t)) # <class 'tuple'>
+t=list(t) 
+print(t) # [28, 9, 1990]
+print(type(t)) #<class 'list'>
+
+t1=(12,'ddsg',213)
+print(t1)
+
+dictionary = {}
+dictionary = {'up': '↑', 'left': '←', 'down': '↓', 'right': '→'}
+print(dictionary) # {'up':'↑', 'left':'←', 'down':'↓', 'right':'→'}
+print(dictionary['left']) # ← типы ключей могут отличаться
+print(dictionary['up']) # ↑ типы ключей могут отличаться
+dictionary['left'] = '⇐'
+print(dictionary['left']) # ⇐
+#print(dictionary['type']) # KeyError: 'type'
+del dictionary['left'] # удаление элемента
+print(dictionary)
+
+for (k,v) in dictionary.items():
+    print(k,v)
+    print(type((k,v)),type(v))
+
+print(dictionary.items()) #список кортежей
